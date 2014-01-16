94506b6e776090b1f75a2a63c26c74a4ee25f378
jeremykohn <jeremykohndev@gmail.com>
2014-01-16 04:56:36 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tryimagemagick::Application.config.secret_key_base = '3a0b5db70d6421eb7256ac47bc1d99e0cf6a043f895e140e387589d363e38bbddae5c37fddff7803ecfeb42e1d54cd6a9e31e0e04509cfa028825fe87497ca3c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tryimagemagick::Application.config.session_store :cookie_store, key: '_tryimagemagick_session'
